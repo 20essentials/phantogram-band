3600cdc2d4e5aff1ecb9d6e72e1ac53e8445e2fb
DominicDev <s0l0estudi4r@gmail.com>
2026-03-06 16:29:00 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'PHANTOGRAM';
+export const nameSongs = [
+	"BLACK OUT DAYS", /*1*/
+	"FALL IN LOVE", /*2*/
+	"CRUEL WORLD", /*3*/
+	"YOU DON’T GET ME HIGH ANYMORE", /*4*/
+	"BLACK OUT DAYS (SLOWED)", /*5*/
+	"SAME OLD BLUES", /*6*/
+	"RUN RUN BLOOD", /*7*/
+	"MISTER IMPOSSIBLE", /*8*/
+	"YOU'RE MINE", /*9*/
+	"BLACK OUT DAYS – FUTURE ISLANDS REMIX", /*10*/
+	"INTO HAPPINESS", /*11*/
+	"IN A SPIRAL", /*12*/
+	"SAME OLD BLUES", /*13*/
+	"NOTHING BUT TROUBLE", /*14*/
+	"BLACK OUT DAYS – (SPED UP)", /*15*/
+	"DEAR GOD", /*16*/
+	"LET ME DOWN", /*17*/
+	"LOVE ME DOWN", /*18*/
+	"PEDESTAL", /*19*/
+	"GLOWING", /*20*/
+]
